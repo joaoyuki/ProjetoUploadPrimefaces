7790409035fc4fd92cd10a76854b6be661381717
jfassis <joao.yuki@gmail.com>
2017-04-07 14:38:21 -0300

    Adicionado código para fazer upload de arquivo PDF e abrir pelo browser.

---
+++ src/com/joao/BasicDocumentViewerBean.java
@@ -0,0 +1,117 @@
+package com.joao;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ComponentSystemEvent;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@ManagedBean  
+@SessionScoped  
+public class BasicDocumentViewerBean implements Serializable{
+
+    private static final long serialVersionUID = 1L;  
+    
+    private StreamedContent content; 
+    
+    UploadedFile file;
+    StreamedContent imagem;
+  
+    public void onPrerender(ComponentSystemEvent event) {  
+  
+        try {  
+      
+            ByteArrayOutputStream out = new ByteArrayOutputStream();  
+  
+            Document document = new Document();  
+            PdfWriter.getInstance(document, out);  
+            document.open();  
+  
+            for (int i = 0; i < 50; i++) {  
+                document.add(new Paragraph("All work and no play makes Jack a dull boy"));  
+            }  
+              
+            document.close();  
+            content = new DefaultStreamedContent(new ByteArrayInputStream(out.toByteArray()), "application/pdf");  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+    }  
+  
+    public StreamedContent getContent() {  
+//        try {  
+//            
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();  
+//  
+//            Document document = new Document();  
+//            PdfWriter.getInstance(document, out);  
+//            document.open();  
+//  
+//            for (int i = 0; i < 50; i++) {  
+//                document.add(new Paragraph("All work and no play makes Jack a dull boy"));  
+//            }  
+//              
+//            document.close();  
+//            content = new DefaultStreamedContent(new ByteArrayInputStream(out.toByteArray()), "application/pdf");  
+//        } catch (Exception e) {  
+//            e.printStackTrace();  
+//        } 
+        return content;  
+    }  
+  
+    public void setContent(StreamedContent content) {  
+        this.content = content;  
+    }  
+    
+    public StreamedContent abrirImagem(){
+        
+            StreamedContent imagem = null;
+			try {
+				imagem = new DefaultStreamedContent(this.file.getInputstream());
+				//setImagem(imagem);
+				setContent(imagem);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            return imagem;
+    }
+    
+	public void fileUploadListener(FileUploadEvent e){
+		// Get uploaded file from the FileUploadEvent
+		
+		this.file = e.getFile();
+		// Print out the information of the file
+		System.out.println("Uploaded File Name Is :: "+file.getFileName()+" :: Uploaded File Size :: "+file.getSize());
+	}
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
+	public StreamedContent getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(StreamedContent imagem) {
+		this.imagem = imagem;
+	}  
+	
+}
